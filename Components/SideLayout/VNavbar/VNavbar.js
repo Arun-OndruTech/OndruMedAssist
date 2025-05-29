e4f6ba1da39b1b57fbd1611d18dedbec2edc3fdc
Arun-OndruTech <aruns@ondrutech.com>
2025-05-29 19:48:01 +0530

    overall bug fixes

--- Components/SideLayout/VNavbar/VNavbar.js
+++ Components/SideLayout/VNavbar/VNavbar.js
@@ -1,51 +1,98 @@
-import classes from './vnavbar.module.css';
-import { FaBell, FaShapes, FaCartPlus, FaFileInvoice, FaChartPie } from 'react-icons/fa';
-import { MdDashboardCustomize } from 'react-icons/md'
-import $ from 'jquery';
-import logo from '../../../Images/MedAssist.png';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { StateContext } from '../../../Context/StateContext';
-import { useContext } from 'react';
+import classes from "./vnavbar.module.css";
+import {
+  FaBell,
+  FaShapes,
+  FaCartPlus,
+  FaFileInvoice,
+  FaBars,
+} from "react-icons/fa";
+import { MdDashboardCustomize } from "react-icons/md";
+import $ from "jquery";
+import logo from "../../../Images/MedAssist.png";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { StateContext } from "../../../Context/StateContext";
+import { useContext, useState, useEffect } from "react";
 
 export default function Navbar() {
+  const router = useRouter();
+  const { state } = useContext(StateContext);
+  const [collapsed, setCollapsed] = useState(false);
 
-    const router = useRouter();
-    const { state } = useContext(StateContext);
-    console.log(state.number_of_notifications);
-
-    // console.log(router.pathname.split('/')[2]);
-
-    $(document).ready(function () {
-        $(`[page*=${!router.pathname.split('/')[2] ? "home" : router.pathname.split('/')[2]}]`).addClass(`${classes.active}`).siblings().removeClass(`${classes.active}`);
-    })
-
-    return (
-        <main className={classes.main}>
-            <nav className={classes.sidebar}>
-                <div className={classes.text}>
-                    <Image
-                        src={logo}
-                        width={150}
-                        alt="logo"
-                        priority="performance"
-                    />
-                </div>
-                <ul>
-                    <li page="home"><Link href='/user'><MdDashboardCustomize style={{ marginRight: "1em" }} /> Dashboard</Link></li>
-                    <li page="items"><Link href='/user/items'><FaShapes style={{ marginRight: "1em" }} />Items</Link></li>
-                    <li page="purchase"><Link href='/user/purchase'><FaCartPlus style={{ marginRight: "1em" }} />Purchase</Link></li>
-                    <li page="sale"><Link href='/user/sale'><FaFileInvoice style={{ marginRight: "1em" }} />Sale</Link></li>
-                    <li className={classes.noti} page="notification">
-                        <Link href='/user/notification'><FaBell style={{ marginRight: "1em" }} />Notification</Link>
-                        {state.number_of_notifications !== 0 && <span className={classes.count}>
-                            {state.number_of_notifications >= 10 ? "9+" : state.number_of_notifications}
-                        </span>}
-                    </li>
-                    {/* <li page="report"><Link href='/user/report'><FaChartPie style={{ marginRight: "1em" }} />Report</Link></li> */}
-                </ul>
-            </nav >
-        </main>
+  useEffect(() => {
+    $(
+      `[page*=${
+        !router.pathname.split("/")[2] ? "home" : router.pathname.split("/")[2]
+      }]`
     )
+      .addClass(`${classes.active}`)
+      .siblings()
+      .removeClass(`${classes.active}`);
+  }, [router.pathname]);
+
+  return (
+    <main className={classes.main}>
+      <nav
+        className={`${classes.sidebar} ${collapsed ? classes.collapsed : ""}`}
+      >
+        <div className={classes.top}>
+          <div className={classes.text}>
+            <Image
+              src={logo}
+              alt="logo"
+              width={collapsed ? 40 : 100}
+              className={classes.logo}
+              priority="performance"
+            />
+          </div>
+          <button
+            className={classes.toggleBtn}
+            onClick={() => setCollapsed(!collapsed)}
+          >
+            <FaBars />
+          </button>
+        </div>
+        <ul>
+          <li page="home">
+            <Link href="/user">
+              <MdDashboardCustomize />
+              {!collapsed && <span>Dashboard</span>}
+            </Link>
+          </li>
+          <li page="items">
+            <Link href="/user/items">
+              <FaShapes />
+              {!collapsed && <span>Items</span>}
+            </Link>
+          </li>
+          <li page="purchase">
+            <Link href="/user/purchase">
+              <FaCartPlus />
+              {!collapsed && <span>Purchase</span>}
+            </Link>
+          </li>
+          <li page="sale">
+            <Link href="/user/sale">
+              <FaFileInvoice />
+              {!collapsed && <span>Sale</span>}
+            </Link>
+          </li>
+          <li className={classes.noti} page="notification">
+            <Link href="/user/notification">
+              <FaBell />
+              {!collapsed && <span>Notification</span>}
+            </Link>
+            {state.number_of_notifications !== 0 && (
+              <span className={classes.count}>
+                {state.number_of_notifications >= 10
+                  ? "9+"
+                  : state.number_of_notifications}
+              </span>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </main>
+  );
 }
