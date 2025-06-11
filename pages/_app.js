446f19572295098807b1b685a32107731cbacd33
Arun-OndruTech <aruns@ondrutech.com>
2025-06-11 13:54:29 +0530

    Fix: remove circular import and simplify app layout logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
-import Home from ".";
 import TopLayout from "../Components/TopLayout/TopLayout";
-import login from "./login";
-import SignUp from "./signup";
 import SideLayout from "../Components/SideLayout/SideLayout";
 import AuthProvider from "../firebase/Context/AuthContext";
 import ProtectedRoute from "../Components/ProtectedRoute/ProtectedRoute";
@@ -10,53 +7,51 @@ import AlreadyLogin from "../Components/AlreadyLogin/AlreadyLogin";
 import { StateContextProvider } from "../Context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-  switch (Component) {
-    case Home: {
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
-              <TopLayout>
-                <Component {...pageProps} />
-              </TopLayout>
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    }
-    case login:
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
+  const pageName = Component.name;
+
+  // Public Pages
+  const isLogin = pageName === "login";
+  const isSignUp = pageName === "SignUp";
+  const isHome = pageName === "Home";
+
+  if (isHome) {
+    return (
+      <AuthProvider>
+        <StateContextProvider>
+          <AlreadyLogin>
+            <TopLayout>
               <Component {...pageProps} />
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    case SignUp:
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <AlreadyLogin>
-              <Component {...pageProps} />
-            </AlreadyLogin>
-          </StateContextProvider>
-        </AuthProvider>
-      );
-    default:
-      return (
-        <AuthProvider>
-          <StateContextProvider>
-            <ProtectedRoute>
-              <SideLayout>
-                <Component {...pageProps} />
-              </SideLayout>
-            </ProtectedRoute>
-          </StateContextProvider>
-        </AuthProvider>
-      );
+            </TopLayout>
+          </AlreadyLogin>
+        </StateContextProvider>
+      </AuthProvider>
+    );
   }
+
+  if (isLogin || isSignUp) {
+    return (
+      <AuthProvider>
+        <StateContextProvider>
+          <AlreadyLogin>
+            <Component {...pageProps} />
+          </AlreadyLogin>
+        </StateContextProvider>
+      </AuthProvider>
+    );
+  }
+
+  // Protected Pages
+  return (
+    <AuthProvider>
+      <StateContextProvider>
+        <ProtectedRoute>
+          <SideLayout>
+            <Component {...pageProps} />
+          </SideLayout>
+        </ProtectedRoute>
+      </StateContextProvider>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
